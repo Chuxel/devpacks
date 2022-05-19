a28de5f631039e02d2915a1374547ead58039655
Chuck Lantz <chuck_lantz@hotmail.com>
2022-05-19 21:26:53 +0000

    Finish npm install

--- devpacks/internal/buildpacks/npminstall/npminstall_builder.go
+++ devpacks/internal/buildpacks/npminstall/npminstall_builder.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/buildpacks/libcnb"
 	"github.com/chuxel/devpacks/internal/buildpacks/base"
+	"github.com/chuxel/devpacks/internal/common"
 )
 
 //go:embed assets/check-symlink.sh
@@ -64,10 +65,20 @@ func (contrib NpmInstallLayerContributor) Contribute(layer libcnb.Layer) (libcnb
 	}
 	hashGen := sha256.New()
 	currentHash := base64.StdEncoding.EncodeToString(hashGen.Sum(packageLockBytes))
-	// If layer already exists, skip if sha256 is the same
+	// Use sha256 to see if layer already exists and is the same so we can reuse
 	layerNodeModules := filepath.Join(layer.Path, "node_modules")
 	if layer.Metadata["sha256"] != nil {
 		if currentHash == fmt.Sprint(layer.Metadata["sha256"]) {
+			// Reuse contents from cache by setting up a symlink
+			// The symlink created here will only apply to the build image and we need it in the launch image.
+			// So add a profile.d script that the launcher will fire to verify, but we need to add some paths
+			script := fmt.Sprintf("#!/bin/bash\nWORKSPACE_FOLDER=\"%s\"\nNPM_INSTALL_LAYER=\"%s\"\n%s", contrib.Context.Application.Path, layer.Path, symlinkScript)
+			layer.Profile.Add("check-symlink", script)
+			layer.LayerTypes = libcnb.LayerTypes{
+				Build:  true,
+				Cache:  true,
+				Launch: true,
+			}
 			return layer, nil
 		} else {
 			// Otherwise remove layer node_modules since we'll need to recreate
@@ -98,28 +109,19 @@ func (contrib NpmInstallLayerContributor) Contribute(layer libcnb.Layer) (libcnb
 		log.Fatal("npm install failed with exit code ", cmd.ProcessState.ExitCode())
 	}
 
-	/*
-		// Move node_modules to layer folder, symlink for other build steps
-		if err := os.MkdirAll(layer.Path, 0755); err != nil {
-			log.Fatal("Unable to create node_modules folder. ", err)
-		}
-		if err := os.Rename(appNodeModules, layerNodeModules); err != nil {
-			log.Fatal("Unable to move node_modules into layer folder. ", err)
-		}
-		if err := os.Symlink(layerNodeModules, appNodeModules); err != nil {
-			log.Fatal("Unable to create node_modules symlink. ", err)
-		}
-	*/
+	// Unfortunately, a "move" doesn't work  since we're across storage devices, so
+	// copy node_modules to layer for future reuse, but mark the layer for caching only
+	if err := os.MkdirAll(layer.Path, 0755); err != nil {
+		log.Fatal("Unable to create node_modules folder. ", err)
+	}
+	common.CpR(appNodeModules, layerNodeModules)
 
-	// The symlink created here will only apply to the build image and we need it in the launch image.
-	// So add a profile.d script that the launcher will fire to verify, but we need to add some paths
-	script := fmt.Sprintf("#!/bin/bash\nWORKSPACE_FOLDER=\"%s\"\nNPM_INSTALL_LAYER=\"%s\"\n%s", contrib.Context.Application.Path, layer.Path, symlinkScript)
-	layer.Profile.Add("check-symlink", script)
-
+	// Only keep the layer around for caching purposes since the
+	// node_modules folder is in the workspace folder in this scenario
 	layer.LayerTypes = libcnb.LayerTypes{
-		Build:  true,
+		Build:  false,
 		Cache:  true,
-		Launch: true,
+		Launch: false,
 	}
 	// Add layer metadata (e.g. hash)
 	layer.Metadata = map[string]interface{}{
